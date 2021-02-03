24ff62ea4ec5b2c6627922860ddeafb53726f52d
iseelise <39001717+elisepitel@users.noreply.github.com>
2021-02-03 07:45:17 +0100

    import hands from hand file

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
 import random
 
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
-
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
-
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
+from hand import rock
+from hand import paper
+from hand import scissors
 
 game_images = [rock, paper, scissors]
 user_input = input("What do you choose ? Type 0 for Rock, 1 for Paper or 2 for Scissors. ")
